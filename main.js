cdd181c22cc81affcc29ab17c7b97218244f9468
Mohit248 <83233932+Mohit248@users.noreply.github.com>
2022-05-20 20:46:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 rise_up = "";
 darkside = "";
 
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+
+
 function preload(){
     rise_up = loadSound("rise_up.mp3");
     darkside = loadSound("darkside.mp3");
@@ -24,3 +31,12 @@ function modelLoaded(){
 function draw(){
     image(video,0,0,200,100);
 }
+
+function gotPoses(results){
+    if(results.length > 0){
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;   
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+    }
+    }
